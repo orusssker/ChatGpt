5f73de4dd7cbc1ae7aba5948303dcc299128ff6b
orusssker <119407871+orusssker@users.noreply.github.com>
2023-03-01 03:11:02 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+
+TOKEN = "6251029616:AAHegWZl5zFSNsGWRU5J5rB7mLX_a2235N4"  # obtained from @BotFather
+OPENAI_TOKEN = "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"  # obtained from https://platform.openai.com/account/api-keys
